8dd7e4145f50abf89e06e070c599ceea13e302c6
Oliver Henrich <oliver.henrich@strath.ac.uk>
2025-01-31 09:29:56 +0000

    Moved initialisation of counter pair coefficient

--- src/CG-DNA/pair_oxdna_excv.cpp
+++ src/CG-DNA/pair_oxdna_excv.cpp
@@ -669,8 +669,6 @@ void PairOxdnaExcv::coeff(int narg, char **arg)
   nlo = ilo;
   nhi = ihi;
 
-  count = 0;
-
   double epsilon_ss_one, sigma_ss_one;
   double cut_ss_ast_one, cut_ss_c_one, b_ss_one;
 
@@ -696,7 +694,8 @@ void PairOxdnaExcv::coeff(int narg, char **arg)
     epsilon_bb_one = utils::numeric(FLERR,arg[8],false,lmp);
     sigma_bb_one = utils::numeric(FLERR,arg[9],false,lmp);
     cut_bb_ast_one = utils::numeric(FLERR,arg[10],false,lmp);
-  } else {
+  }
+  else {
     if (comm->me == 0) {
       PotentialFileReader reader(lmp, arg[2], "oxdna potential", " (excv)");
       char * line;
@@ -710,17 +709,17 @@ void PairOxdnaExcv::coeff(int narg, char **arg)
           potential_name = values.next_string();
           if (iloc == arg[0] && jloc == arg[1] && potential_name == "excv") {
             // Excluded volume interaction
-            // LJ parameters
+            // LJ backbone-backbone parameters
             epsilon_ss_one = values.next_double();
             sigma_ss_one = values.next_double();
             cut_ss_ast_one = values.next_double();
 
-            // LJ parameters
+            // LJ backbone-base parameters
             epsilon_sb_one = values.next_double();
             sigma_sb_one = values.next_double();
             cut_sb_ast_one = values.next_double();
 
-            // LJ parameters
+            // LJ base-base parameters
             epsilon_bb_one = values.next_double();
             sigma_bb_one = values.next_double();
             cut_bb_ast_one = values.next_double();
@@ -749,6 +748,9 @@ void PairOxdnaExcv::coeff(int narg, char **arg)
     MPI_Bcast(&cut_bb_ast_one, 1, MPI_DOUBLE, 0, world);
   }
 
+  // backbone-backbone
+  count = 0;
+
   // smoothing - determined through continuity and differentiability
   b_ss_one = 4.0/sigma_ss_one
       *(6.0*pow(sigma_ss_one/cut_ss_ast_one,7)-12.0*pow(sigma_ss_one/cut_ss_ast_one,13))
@@ -759,13 +761,18 @@ void PairOxdnaExcv::coeff(int narg, char **arg)
       - 2.0*4.0*(pow(sigma_ss_one/cut_ss_ast_one,12)-pow(sigma_ss_one/cut_ss_ast_one,6))
       /(4.0/sigma_ss_one*(6.0*pow(sigma_ss_one/cut_ss_ast_one,7)-12.0*pow(sigma_ss_one/cut_ss_ast_one,13)));
 
-  for (int i = ilo; i <= ihi; i++) {
-    for (int j = MAX(jlo,i); j <= jhi; j++) {
+  // backbone-backbone parameters depending on base step
+  for (int i = nlo; i <= nhi; i++) {
+    for (int j = nlo; j <= nhi; j++) {
       epsilon_ss[i][j] = epsilon_ss_one;
       sigma_ss[i][j] = sigma_ss_one;
       cut_ss_ast[i][j] = cut_ss_ast_one;
       b_ss[i][j] = b_ss_one;
       cut_ss_c[i][j] = cut_ss_c_one;
+      lj1_ss[i][j] = 4.0 * epsilon_ss[i][j] * pow(sigma_ss[i][j],12.0);
+      lj2_ss[i][j] = 4.0 * epsilon_ss[i][j] * pow(sigma_ss[i][j],6.0);
+      cutsq_ss_ast[i][j] = cut_ss_ast[i][j]*cut_ss_ast[i][j];
+      cutsq_ss_c[i][j]  = cut_ss_c[i][j]*cut_ss_c[i][j];
       setflag[i][j] = 1;
       count++;
     }
@@ -773,6 +780,7 @@ void PairOxdnaExcv::coeff(int narg, char **arg)
 
   if (count == 0) error->all(FLERR,"Incorrect args for pair coefficients in oxdna/excv");
 
+  // backbone-base
   count = 0;
 
   // smoothing - determined through continuity and differentiability
@@ -785,18 +793,24 @@ void PairOxdnaExcv::coeff(int narg, char **arg)
       - 2.0*4.0*(pow(sigma_sb_one/cut_sb_ast_one,12)-pow(sigma_sb_one/cut_sb_ast_one,6))
       /(4.0/sigma_sb_one*(6.0*pow(sigma_sb_one/cut_sb_ast_one,7)-12.0*pow(sigma_sb_one/cut_sb_ast_one,13)));
 
-  for (int i = ilo; i <= ihi; i++) {
-    for (int j = MAX(jlo,i); j <= jhi; j++) {
+  // backbone-base parameters depending on base step
+  for (int i = nlo; i <= nhi; i++) {
+    for (int j = nlo; j <= nhi; j++) {
       epsilon_sb[i][j] = epsilon_sb_one;
       sigma_sb[i][j] = sigma_sb_one;
       cut_sb_ast[i][j] = cut_sb_ast_one;
       b_sb[i][j] = b_sb_one;
       cut_sb_c[i][j] = cut_sb_c_one;
+      lj1_sb[i][j] = 4.0 * epsilon_sb[i][j] * pow(sigma_sb[i][j],12.0);
+      lj2_sb[i][j] = 4.0 * epsilon_sb[i][j] * pow(sigma_sb[i][j],6.0);
+      cutsq_sb_ast[i][j] = cut_sb_ast[i][j]*cut_sb_ast[i][j];
+      cutsq_sb_c[i][j]  = cut_sb_c[i][j]*cut_sb_c[i][j];
       setflag[i][j] = 1;
       count++;
     }
   }
 
+  // backbone-base parameters depending on tetramer
   for (int i = nlo; i <= nhi; i++) {
     for (int j = nlo; j <= nhi; j++) {
       for (int k = nlo; k <= nhi; k++) {
@@ -816,6 +830,7 @@ void PairOxdnaExcv::coeff(int narg, char **arg)
 
   if (count == 0) error->all(FLERR,"Incorrect args for pair coefficients in oxdna/excv");
 
+  // base-base
   count = 0;
 
   // smoothing - determined through continuity and differentiability
@@ -828,18 +843,24 @@ void PairOxdnaExcv::coeff(int narg, char **arg)
       - 2.0*4.0*(pow(sigma_bb_one/cut_bb_ast_one,12)-pow(sigma_bb_one/cut_bb_ast_one,6))
       /(4.0/sigma_bb_one*(6.0*pow(sigma_bb_one/cut_bb_ast_one,7)-12.0*pow(sigma_bb_one/cut_bb_ast_one,13)));
 
-  for (int i = ilo; i <= ihi; i++) {
-    for (int j = MAX(jlo,i); j <= jhi; j++) {
+  // base-base parameters depending on base step
+  for (int i = nlo; i <= nhi; i++) {
+    for (int j = nlo; j <= nhi; j++) {
       epsilon_bb[i][j] = epsilon_bb_one;
       sigma_bb[i][j] = sigma_bb_one;
       cut_bb_ast[i][j] = cut_bb_ast_one;
       b_bb[i][j] = b_bb_one;
       cut_bb_c[i][j] = cut_bb_c_one;
+      lj1_bb[i][j] = 4.0 * epsilon_bb[i][j] * pow(sigma_bb[i][j],12.0);
+      lj2_bb[i][j] = 4.0 * epsilon_bb[i][j] * pow(sigma_bb[i][j],6.0);
+      cutsq_bb_ast[i][j] = cut_bb_ast[i][j]*cut_bb_ast[i][j];
+      cutsq_bb_c[i][j]  = cut_bb_c[i][j]*cut_bb_c[i][j];
       setflag[i][j] = 1;
       count++;
     }
   }
 
+  // base-base parameters depending on tetramer
   for (int i = nlo; i <= nhi; i++) {
     for (int j = nlo; j <= nhi; j++) {
       for (int k = nlo; k <= nhi; k++) {
@@ -886,62 +907,6 @@ double PairOxdnaExcv::init_one(int i, int j)
   if (offset_flag) {
     error->all(FLERR,"Offset not supported in oxDNA");
   }
-
-  epsilon_ss[j][i] = epsilon_ss[i][j];
-  sigma_ss[j][i] = sigma_ss[i][j];
-  cut_ss_ast[j][i] = cut_ss_ast[i][j];
-  cut_ss_c[j][i] = cut_ss_c[i][j];
-  b_ss[j][i] = b_ss[i][j];
-
-  epsilon_sb[j][i] = epsilon_sb[i][j];
-  sigma_sb[j][i] = sigma_sb[i][j];
-  cut_sb_ast[j][i] = cut_sb_ast[i][j];
-  cut_sb_c[j][i] = cut_sb_c[i][j];
-  b_sb[j][i] = b_sb[i][j];
-
-  epsilon_bb[j][i] = epsilon_bb[i][j];
-  sigma_bb[j][i] = sigma_bb[i][j];
-  cut_bb_ast[j][i] = cut_bb_ast[i][j];
-  cut_bb_c[j][i] = cut_bb_c[i][j];
-  b_bb[j][i] = b_bb[i][j];
-
-  // excluded volume auxiliary parameters
-
-  lj1_ss[i][j] = 4.0 * epsilon_ss[i][j] * pow(sigma_ss[i][j],12.0);
-  lj2_ss[i][j] = 4.0 * epsilon_ss[i][j] * pow(sigma_ss[i][j],6.0);
-
-  lj1_sb[i][j] = 4.0 * epsilon_sb[i][j] * pow(sigma_sb[i][j],12.0);
-  lj2_sb[i][j] = 4.0 * epsilon_sb[i][j] * pow(sigma_sb[i][j],6.0);
-
-  lj1_bb[i][j] = 4.0 * epsilon_bb[i][j] * pow(sigma_bb[i][j],12.0);
-  lj2_bb[i][j] = 4.0 * epsilon_bb[i][j] * pow(sigma_bb[i][j],6.0);
-
-  lj1_ss[j][i] = lj1_ss[i][j];
-  lj2_ss[j][i] = lj2_ss[i][j];
-
-  lj1_sb[j][i] = lj1_sb[i][j];
-  lj2_sb[j][i] = lj2_sb[i][j];
-
-  lj1_bb[j][i] = lj1_bb[i][j];
-  lj2_bb[j][i] = lj2_bb[i][j];
-
-  cutsq_ss_ast[i][j] = cut_ss_ast[i][j]*cut_ss_ast[i][j];
-  cutsq_ss_c[i][j]  = cut_ss_c[i][j]*cut_ss_c[i][j];
-
-  cutsq_sb_ast[i][j] = cut_sb_ast[i][j]*cut_sb_ast[i][j];
-  cutsq_sb_c[i][j]  = cut_sb_c[i][j]*cut_sb_c[i][j];
-
-  cutsq_bb_ast[i][j] = cut_bb_ast[i][j]*cut_bb_ast[i][j];
-  cutsq_bb_c[i][j]  = cut_bb_c[i][j]*cut_bb_c[i][j];
-
-  cutsq_ss_ast[j][i] = cutsq_ss_ast[i][j];
-  cutsq_ss_c[j][i]  = cutsq_ss_c[i][j];
-
-  cutsq_sb_ast[j][i] = cutsq_sb_ast[i][j];
-  cutsq_sb_c[j][i]  = cutsq_sb_c[i][j];
-
-  cutsq_bb_ast[j][i] = cutsq_bb_ast[i][j];
-  cutsq_bb_c[j][i]  = cutsq_bb_c[i][j];
 
   // set the master list distance cutoff
   return cut_ss_c[i][j];
