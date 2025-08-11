91cc6ad4df179d0060ec0f2bbe546fac1d6c6faf
Henry Agnew <hagnew@ucsd.edu>
2025-08-10 22:12:45 -0700

    Added MBX examples

---
+++ examples/PACKAGES/mbx/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LAMMPS_HOME=$HOME/software/LAMMPS-stable
+
+MPI_PROC=1        # number of mpi processes
+OMP_NUM_THREADS=1 # number of openmp threads per mpi process
+
+# optional command line arguments to override the default mpi and openmp settings
+if [ $# -eq 2 ]; then
+    MPI_PROC=$1
+    OMP_NUM_THREADS=$2
+fi
+
+echo "Running MBX+LAMMPS using MPI=$MPI_PROC and OMP=$OMP_NUM_THREADS"
+
+export OMP_NUM_THREADS
+mpirun -np "${MPI_PROC}" "$LAMMPS_HOME"/build/lmp -in lammps.in
+
+if [ -s log.lammps ] && grep -q "Total wall time" log.lammps; then
+    date | tee -a performance.txt
+    echo "<$(hostname -s)>" | tee -a performance.txt
+    grep 'ns/day' log.lammps | tee -a performance.txt
+    grep '% CPU use' log.lammps | tee -a performance.txt
+    grep 'Total wall time' log.lammps | tee -a performance.txt
+    echo "-----------------------------" | tee -a performance.txt
+fi
