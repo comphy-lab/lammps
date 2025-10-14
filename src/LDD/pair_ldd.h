b13ceabb5e82997d3a9065625f127a815ebe8ee1
Maria Lesniewski <mjl6766@psu.edu>
2025-10-14 16:08:32 -0400

    Updated virtual vs override syntax in pair_ldd.h, added comments for the different structures/fields created in pair_ldd, verified unit tests still work

--- src/LDD/pair_ldd.h
+++ src/LDD/pair_ldd.h
@@ -40,26 +40,26 @@ class PairLdd: public Pair {
  public:
   PairLdd(class LAMMPS *);
   virtual ~PairLdd();
-  virtual void compute(int, int);
-  void settings(int, char **);
-  void coeff(int, char **);
-  void init_style();
-  double init_one(int, int);
-  void write_restart_settings(FILE *);
-  void read_restart_settings(FILE *);
-  double single(int, int, int, int, double, double, double, double &);
+  void compute(int, int) override;
+  void settings(int, char **) override;
+  void coeff(int, char **) override;
+  void init_style() override;
+  double init_one(int, int) override;
+  void write_restart_settings(FILE *) override;
+  void read_restart_settings(FILE *) override;
+  double single(int, int, int, int, double, double, double, double &) override;
 
   /* I do this the same way it's done in force.h */
 
-  char *indicator_style;
-  class LddIndicator *indicator;
+  char *indicator_style; // key for LDD indicator map
+  class LddIndicator *indicator; // w(r) and all associated info
   typedef LddIndicator *(*IndicatorCreator)(LAMMPS *);
   typedef std::map<std::string,IndicatorCreator> IndicatorCreatorMap;
   IndicatorCreatorMap *indicator_map;
   class LddIndicator * new_indicator(std::string wtype);
 
-  char *potential_style;
-  class LddPotential *potential;
+  char *potential_style; // key for LDD potential U_x map
+  class LddPotential *potential; // U_x and all associated info
   typedef LddPotential *(*PotentialCreator)(LAMMPS *);
   typedef std::map<std::string,PotentialCreator> PotentialCreatorMap;
   PotentialCreatorMap *potential_map;
@@ -76,18 +76,18 @@ class PairLdd: public Pair {
   void unpack_reverse_comm(int, int *, double *);
 
  protected:
-  double cut_LDD_global;
-  double **cut;
-  bool **self_interaction; 
-  bool **ignore_pair;
-  bool *ignore_me; 
-  bool **bGradient; 
+  double cut_LDD_global; // longest interaction cutoff obtained from pair_style ldd
+  double **cut; // cutoffs passed in pair_coeff ldd commands. dim n_types x n_types
+  bool **self_interaction; // Self interaction settings from pair_coeff. dim n_types x n_types
+  bool **ignore_pair; // Ignored Potential list. dim n_types x n_types
+  bool *ignore_me;  // Totally ignored central type list. dim_ntypes 
+  bool **bGradient; // List of pair types a surrounded by b that also have a gradient interaction. dim n_types x n_types
 
-  LddIndicator ***Inds; 
-  LddPotential ***Potls; 
-  LddPotential ***GradPotls; 
+  LddIndicator ***Inds; //The address of an n_type x n_type structure, holding all a|b indicator info 
+  LddPotential ***Potls; //The address of an n_type x n_type structure, holding all a|b U_rho info
+  LddPotential ***GradPotls; //The address of an n_type x n_type structure, holding all a|b U_{\nabla} info
  
-  virtual void allocate();
+  void allocate();
   void ErrorDoubleKeyword(const char *);
   void ErrorNumKeywordArgs(const char *, const char *);
 
