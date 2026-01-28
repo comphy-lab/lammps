f0779fdbbc2666f08454412ce2606437d928c5b5
aknoll <aknoll@tc2-pc007.theochem2.ruhr-uni-bochum.de>
2026-01-28 11:49:00 +0100

    Only check whether the RuNNer lib dir exists if the user defined it.

--- cmake/Modules/Packages/ML-RUNNER.cmake
+++ cmake/Modules/Packages/ML-RUNNER.cmake
@@ -151,9 +151,12 @@ if(DOWNLOAD_RUNNER)
 else()
   message(STATUS "DOWNLOAD_RUNNER is OFF. Looking for a pre-compiled RuNNer.")
 
-  # Check if the directory specified by RUNNER_LIB_DIR exists
-  if(NOT IS_DIRECTORY ${RUNNER_LIB_DIR})
-    message(FATAL_ERROR "The directory specified by RUNNER_LIB_DIR does not exist: ${RUNNER_LIB_DIR}")
+  # Check if the directory specified by RUNNER_LIB_DIR exists, if the
+  # user defined it.
+  if(DEFINED ${RUNNER_LIB_DIR})
+    if(NOT IS_DIRECTORY ${RUNNER_LIB_DIR})
+      message(FATAL_ERROR "The directory specified by RUNNER_LIB_DIR does not exist: ${RUNNER_LIB_DIR}")
+    endif()
   endif()
 
   # Determine the correct file extension based on whether the library is shared or not.
