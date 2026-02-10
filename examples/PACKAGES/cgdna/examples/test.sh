8da6721e615f523afe2535022d4e793b5d2bdc86
Oliver Henrich <oliver.henrich@strath.ac.uk>
2026-02-10 16:25:25 +0000

    Repaired and shortened oxDNA and oxDNA2 regression tests

--- examples/PACKAGES/cgdna/examples/test.sh
+++ examples/PACKAGES/cgdna/examples/test.sh
@@ -8,17 +8,17 @@ SRCDIR=$LMPDIR/src
 EXDIR=$LMPDIR/examples/PACKAGES/cgdna/examples/lj_units
 
 if [ $# -eq 1 ] && [ $1 = run ]; then
-  echo '# Compiling executable in' $SRCDIR | tee -a $EXDIR/test.log
+#  echo '# Compiling executable in' $SRCDIR | tee -a $EXDIR/test.log
 
-  cd $SRCDIR
-  make clean-all | tee -a $EXDIR/test.log
-  make purge | tee -a $EXDIR/test.log
-  make pu | tee -a $EXDIR/test.log
-  make ps | tee -a $EXDIR/test.log
-  make -j14 mpi | tee -a $EXDIR/test.log
+#  cd $SRCDIR
+#  make clean-all | tee -a $EXDIR/test.log
+#  make purge | tee -a $EXDIR/test.log
+#  make pu | tee -a $EXDIR/test.log
+#  make ps | tee -a $EXDIR/test.log
+#  make -j14 mpi | tee -a $EXDIR/test.log
 
   ######################################################
-  printf '\n# Running oxDNA duplex1 test\n' | tee -a $EXDIR/test.log
+  printf '\n# Running oxDNA duplex1 NVE test\n' | tee -a $EXDIR/test.log
   cd $EXDIR/oxDNA/duplex1
   mkdir test
   cd test
@@ -123,7 +123,7 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
   ' 2>&1 | tee -a $EXDIR/test.log
 
   ######################################################
-  printf '\n# Running oxDNA duplex2 test\n' | tee -a $EXDIR/test.log
+  printf '\n# Running oxDNA duplex2 NVT test\n' | tee -a $EXDIR/test.log
   cd $EXDIR/oxDNA/duplex2
   mkdir test
   cd test
@@ -223,14 +223,118 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
       }
     }
     END {
-      if (failed == 0) print "# 4 MPI-tasks passed\n"
+      if (failed == 0) print "# 4 MPI-tasks passed"
     }
   ' 2>&1 | tee -a $EXDIR/test.log
 
   ######################################################
+  printf '\n# Running oxDNA potential file NVE test\n' | tee -a $EXDIR/test.log
+  cd $EXDIR/oxDNA/potential_file
+  mkdir test
+  cd test
+  cp $SRCDIR/lmp_mpi .
+  cp ../in.duplex1 .
+  cp ../data.duplex1 .
+  cp ../oxdna_lj.cgdna .
+
+  ### 1 MPI-task ###
+  mpirun -np 1 ./lmp_mpi -in in.duplex1 > /dev/null
+  mv log.lammps log.$DATE.duplex1.g++.1
+  grep -e '[0-9]  ekin' log.$DATE.duplex1.g++.1 > e_test.1.dat
+  grep -e '[0-9]  ekin' ../log*1 > e_ref.1.dat
+
+  paste e_ref.1.dat e_test.1.dat |
+
+  awk -v tol="$REL_TOL" '
+    failed == 0 {
+      diff = ($4-$20)/$4
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $4, $20, diff, tol
+        printf "# 1 MPI-task FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($8-$24)/$8
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $8, $24, diff, tol
+        printf "# 1 MPI-task FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($12-$28)/$12
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $12, $28, diff, tol
+        printf "# 1 MPI-task FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($16-$32)/$16
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $16, $32, diff, tol
+        printf "# 1 MPI-task FAILED\n"
+        failed = 1
+        exit 1
+      }
+    }
+    END {
+      if (failed == 0) print "# 1 MPI-task passed"
+    }
+  ' 2>&1 | tee -a $EXDIR/test.log
+
+  ### 4 MPI-tasks ###
+  mpirun -np 4 ./lmp_mpi -in in.duplex1 > /dev/null
+  mv log.lammps log.$DATE.duplex1.g++.4
+  grep -e '[0-9]  ekin' log.$DATE.duplex1.g++.4 > e_test.4.dat
+  grep -e '[0-9]  ekin' ../log*4 > e_ref.4.dat
+
+  paste e_ref.4.dat e_test.4.dat |
+
+  awk -v tol="$REL_TOL" '
+    failed == 0 {
+      diff = ($4-$20)/$4
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $4, $20, diff, tol
+        printf "# 4 MPI-tasks FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($8-$24)/$8
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $8, $24, diff, tol
+        printf "# 4 MPI-tasks FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($12-$28)/$12
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $12, $28, diff, tol
+        printf "# 4 MPI-tasks FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($16-$32)/$16
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $16, $32, diff, tol
+        printf "# 4 MPI-tasks FAILED\n"
+        failed = 1
+        exit 1
+      }
+    }
+    END {
+      if (failed == 0) print "# 4 MPI-tasks passed"
+    }
+  ' 2>&1 | tee -a $EXDIR/test.log
 
   ######################################################
-  printf '\n# Running oxDNA2 duplex1 test\n' | tee -a $EXDIR/test.log
+  printf '\n# Running oxDNA2 duplex1 NVE test\n' | tee -a $EXDIR/test.log
   cd $EXDIR/oxDNA2/duplex1
   mkdir test
   cd test
@@ -335,7 +439,7 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
   ' 2>&1 | tee -a $EXDIR/test.log
 
   ######################################################
-  printf '\n# Running oxDNA2 duplex2 test\n' | tee -a $EXDIR/test.log
+  printf '\n# Running oxDNA2 duplex2 NVT test\n' | tee -a $EXDIR/test.log
   cd $EXDIR/oxDNA2/duplex2
   mkdir test
   cd test
@@ -440,7 +544,7 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
   ' 2>&1 | tee -a $EXDIR/test.log
 
   ######################################################
-  printf '\n# Running oxDNA2 duplex3 test\n' | tee -a $EXDIR/test.log
+  printf '\n# Running oxDNA2 duplex3 NVE test\n' | tee -a $EXDIR/test.log
   cd $EXDIR/oxDNA2/duplex3
   mkdir test
   cd test
@@ -545,7 +649,7 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
   ' 2>&1 | tee -a $EXDIR/test.log
 
   ######################################################
-  printf '\n# Running oxDNA2 unique_bp test\n' | tee -a $EXDIR/test.log
+  printf '\n# Running oxDNA2 unique_bp NVT test\n' | tee -a $EXDIR/test.log
   cd $EXDIR/oxDNA2/unique_bp
   mkdir test
   cd test
@@ -746,7 +850,7 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
   ' 2>&1 | tee -a $EXDIR/test.log
 
   ######################################################
-  printf '\n# Running oxDNA2 dsring test\n' | tee -a $EXDIR/test.log
+  printf '\n# Running oxDNA2 dsring NVE test\n' | tee -a $EXDIR/test.log
   cd $EXDIR/oxDNA2/dsring
   mkdir test
   cd test
@@ -851,7 +955,113 @@ if [ $# -eq 1 ] && [ $1 = run ]; then
   ' 2>&1 | tee -a $EXDIR/test.log
 
   ######################################################
-  printf '\n# Running oxRNA2 duplex2 test\n' | tee -a $EXDIR/test.log
+  printf '\n# Running oxDNA2 potential file NVE test\n' | tee -a $EXDIR/test.log
+  cd $EXDIR/oxDNA2/potential_file
+  mkdir test
+  cd test
+  cp $SRCDIR/lmp_mpi .
+  cp ../in.duplex1 .
+  cp ../data.duplex1 .
+  cp ../oxdna2_lj.cgdna .
+
+  ### 1 MPI-task ###
+  mpirun -np 1 ./lmp_mpi -in in.duplex1 > /dev/null
+  mv log.lammps log.$DATE.duplex1.g++.1
+  grep -e '[0-9]  ekin' log.$DATE.duplex1.g++.1 > e_test.1.dat
+  grep -e '[0-9]  ekin' ../log*1 > e_ref.1.dat
+
+  paste e_ref.1.dat e_test.1.dat |
+
+  awk -v tol="$REL_TOL" '
+    failed == 0 {
+      diff = ($4-$20)/$4
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $4, $20, diff, tol
+        printf "# 1 MPI-task FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($8-$24)/$8
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $8, $24, diff, tol
+        printf "# 1 MPI-task FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($12-$28)/$12
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $12, $28, diff, tol
+        printf "# 1 MPI-task FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($16-$32)/$16
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $16, $32, diff, tol
+        printf "# 1 MPI-task FAILED\n"
+        failed = 1
+        exit 1
+      }
+    }
+    END {
+      if (failed == 0) print "# 1 MPI-task passed"
+    }
+  ' 2>&1 | tee -a $EXDIR/test.log
+
+  ### 4 MPI-tasks ###
+  mpirun -np 4 ./lmp_mpi -in in.duplex1 > /dev/null
+  mv log.lammps log.$DATE.duplex1.g++.4
+  grep -e '[0-9]  ekin' log.$DATE.duplex1.g++.4 > e_test.4.dat
+  grep -e '[0-9]  ekin' ../log*4 > e_ref.4.dat
+
+  paste e_ref.4.dat e_test.4.dat |
+
+  awk -v tol="$REL_TOL" '
+    failed == 0 {
+      diff = ($4-$20)/$4
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $4, $20, diff, tol
+        printf "# 4 MPI-tasks FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($8-$24)/$8
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $8, $24, diff, tol
+        printf "# 4 MPI-tasks FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($12-$28)/$12
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $12, $28, diff, tol
+        printf "# 4 MPI-tasks FAILED\n"
+        failed = 1
+        exit 1
+      }
+      diff = ($16-$32)/$16
+      if (diff < 0) diff = -diff
+      if (diff > tol) {
+        printf "# Line %d: %g vs %g (relative difference = %g > %g)\n", NR, $16, $32, diff, tol
+        printf "# 4 MPI-tasks FAILED\n"
+        failed = 1
+        exit 1
+      }
+    }
+    END {
+      if (failed == 0) print "# 4 MPI-tasks passed"
+    }
+  ' 2>&1 | tee -a $EXDIR/test.log
+
+  ######################################################
+  printf '\n# Running oxRNA2 duplex2 NVE test\n' | tee -a $EXDIR/test.log
   cd $EXDIR/oxRNA2/duplex2
   mkdir test
   cd test
@@ -963,11 +1173,13 @@ elif [ $# -eq 1 ] && [ $1 = clean ]; then
   echo '# Deleting test directories'
   rm -rf $EXDIR/oxDNA/duplex1/test
   rm -rf $EXDIR/oxDNA/duplex2/test
+  rm -rf $EXDIR/oxDNA/potential_file/test
   rm -rf $EXDIR/oxDNA2/duplex1/test
   rm -rf $EXDIR/oxDNA2/duplex2/test
   rm -rf $EXDIR/oxDNA2/duplex3/test
   rm -rf $EXDIR/oxDNA2/unique_bp/test
   rm -rf $EXDIR/oxDNA2/dsring/test
+  rm -rf $EXDIR/oxDNA2/potential_file/test
   rm -rf $EXDIR/oxRNA2/duplex2/test
   rm -rf $EXDIR/test.log
   echo '# Done'
