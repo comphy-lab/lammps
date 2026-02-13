1c9b28b4c56833afcf3403d8b3e3919bb59b947d
Axel Kohlmeyer <akohlmey@gmail.com>
2026-02-12 19:13:55 -0500

    improve tarball creation script

--- cmake/packaging/build_tarball.sh
+++ cmake/packaging/build_tarball.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+set -euo pipefail
 
 APP_NAME=lammps
 DOCDIR="$1"
-VERSION="$2"
-BRANCH=$(git branch --show-current)
-DESTDIR=${PWD}
+DESTDIR="${PWD}"
 
 echo "Delete tar files, if they exist"
-rm -rvf ${PWD}/lammps-src-*.tar.gz
-pushd ${DOCDIR}/..
-git archive --output=${DESTDIR}/${BRANCH}.tar --prefix=lammps-${VERSION}/ HEAD
+rm -rvf "${PWD}"/lammps-src-*.tar.gz
+pushd "${DOCDIR}/.."
+VERSION=$(grep LAMMPS_VERSION src/version.h | sed 's/^.*LAMMPS_VERSION //' | tr -d \" | tr -d \ )
+TARNAME=lammps-src-${VERSION}.tar
+TARPATH="${DESTDIR}/${TARNAME}"
+git archive --output="${TARPATH}" --prefix=lammps-${VERSION}/ HEAD
 
-cd ${DOCDIR}
+cd "${DOCDIR}"
 make clean-all
 make html pdf
-tar -rf ${DESTDIR}/${BRANCH}.tar --transform "s,^,lammps-${VERSION}/doc/," html Manual.pdf
+rm html/index.html
+cp html/Manual.html html/index.html
+tar -rf "${TARPATH}" --owner=root --group=root --transform "s,^,lammps-${VERSION}/doc/," html Manual.pdf
 popd
-mv ${BRANCH}.tar lammps-src-${VERSION}.tar
-gzip -9v lammps-src-${VERSION}.tar
+gzip -9v "${TARNAME}"
 exit 0
