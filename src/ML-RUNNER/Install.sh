9f91505fc8c041553515395b61e6fe2d99903da8
Alexander Knoll <alexander.knoll@rub.de>
2026-04-23 09:49:59 +0200

    Simplify Install.sh

--- src/ML-RUNNER/Install.sh
+++ src/ML-RUNNER/Install.sh
@@ -1,9 +1,5 @@
 # Install/unInstall package files in LAMMPS
 # mode = 0/1/2 for uninstall/install/update
 
-mode=$1
-
-if (test $mode = 1 || test $mode = 2 || test $mode = 3) then
-  echo "The ML-RUNNER package does not support the legacy build system. Please build LAMMPS with CMake instead."
-  exit 1
-fi
+echo "The ML-RUNNER package does not support the legacy build system. Please build LAMMPS with CMake instead."
+exit 1
