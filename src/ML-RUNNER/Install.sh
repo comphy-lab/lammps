9f3dd860cf7ee3f7d3bda692f44dd946d40331fd
Axel Kohlmeyer <akohlmey@gmail.com>
2026-04-23 07:51:02 -0400

    follow install script convention from other packages

--- src/ML-RUNNER/Install.sh
+++ src/ML-RUNNER/Install.sh
@@ -3,7 +3,25 @@
 
 mode=$1
 
-if (test $mode = 1 || test $mode = 2 || test $mode = 3) then
+# enforce using portable C locale
+LC_ALL=C
+export LC_ALL
+
+action () {
+  if (test $mode = 0) then
+    rm -f ../$1
+  fi
+}
+
+# all package files with no dependencies
+
+for file in *.cpp *.h; do
+  test -f ${file} && action $file
+done
+
+# edit 2 Makefile.package files to include/exclude package info
+
+if (test $1 = 1 || test $1 = 2) then
   echo "The ML-RUNNER package does not support the legacy build system. Please build LAMMPS with CMake instead."
   exit 1
 fi
