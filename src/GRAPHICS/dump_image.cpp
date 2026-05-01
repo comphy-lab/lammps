145c13d89039413228d7eb42f4e19d35dc8e6f46
Axel Kohlmeyer <akohlmey@gmail.com>
2026-05-01 01:02:00 -0400

    add support for saving and loading color and light settings with JSON files

--- src/GRAPHICS/dump_image.cpp
+++ src/GRAPHICS/dump_image.cpp
@@ -34,6 +34,7 @@
 #include "image.h"
 #include "image_objects.h"
 #include "input.h"
+#include "json.h"
 #include "math_const.h"
 #include "math_extra.h"
 #include "memory.h"
@@ -52,12 +53,14 @@
 #include "region_plane.h"
 #include "region_prism.h"
 #include "region_sphere.h"
+#include "safe_pointers.h"
 #include "thermo.h"
 #include "tokenizer.h"
 #include "update.h"
 #include "variable.h"
 
 #include <cctype>
+#include <cinttypes>
 #include <cmath>
 #include <cstring>
 
@@ -80,21 +83,21 @@ enum { OFF = 0, CENTER, LOWERLEFT, LOWERRIGHT, UPPERLEFT, UPPERRIGHT };
 
 const std::vector<std::string> default_colors{
     "darkgray",  "red",      "forestgreen", "blue",       "gold", "cyan",
-    "magenta",   "silver",   "orange",      "lime", "gray",   "darkred",
+    "magenta",   "silver",   "orange",      "lime",       "gray", "darkred",
     "darkgreen", "darkblue", "darkcyan",    "darkmagenta"};
 
 //  convenience functions to change and restore lighting, assuming uncolored light
 
-struct savedColors {
+struct savedLights {
   double ambient;
   double key;
   double fill;
   double back;
 };
 
-savedColors reset_lighting(Image *image, double ambient, double key, double fill, double back)
+savedLights reset_lighting(Image *image, double ambient, double key, double fill, double back)
 {
-  savedColors saved;
+  savedLights saved;
   saved.ambient = image->ambientColor[0];
   image->ambientColor[0] = image->ambientColor[1] = image->ambientColor[2] = ambient;
   saved.key = image->keyLightColor[0];
@@ -106,7 +109,7 @@ savedColors reset_lighting(Image *image, double ambient, double key, double fill
   return saved;
 }
 
-void restore_lighting(const savedColors &saved, Image *image)
+void restore_lighting(const savedLights &saved, Image *image)
 {
   image->ambientColor[0] = image->ambientColor[1] = image->ambientColor[2] = saved.ambient;
   image->keyLightColor[0] = image->keyLightColor[1] = image->keyLightColor[2] = saved.key;
@@ -2847,6 +2850,7 @@ int DumpImage::modify_param(int narg, char **arg)
     return 3;
   }
 
+  // clang-format on
   if (strcmp(arg[0], "lights") == 0) {
     if (narg < 5) utils::missing_cmd_args(FLERR, "dump_modify lights", error);
     double ambient = utils::numeric(FLERR, arg[1], false, lmp);
@@ -2865,6 +2869,170 @@ int DumpImage::modify_param(int narg, char **arg)
     restore_lighting({ambient, key, fill, back}, image);
 
     return 5;
+  }
+
+  if (strcmp(arg[0], "savecolors") == 0) {
+    if (narg < 2) utils::missing_cmd_args(FLERR, "dump_modify savecolors", error);
+
+    if (comm->me == 0) {
+      SafeFilePtr fp = fopen(arg[1], "w");
+      if (fp == nullptr)
+        error->one(FLERR, argoff + 1, "Cannot open color JSON file {} for writing: {}", filename,
+                   utils::getsyserror());
+
+      json colordata;
+      colordata["application"] = "LAMMPS";
+      colordata["format"] = "colors";
+      colordata["revision"] = 1;
+      colordata["title"] = "per-type colors for dump image";
+      colordata["schema"] = "https://download.lammps.org/json/color-schema.json";
+      // store per-type colors
+      std::unordered_set<std::string> usedcolors;
+      for (int i = 1; i <= atom->ntypes; ++i) {
+        // lookup color name but avoid using the same name twice
+        auto name = image->rgb2color(colortype[i]);
+        if (name.empty() || (usedcolors.find(name) != usedcolors.end()))
+          name = fmt::format("type{}", i);
+        usedcolors.insert(name);
+
+        colordata["colors"][i - 1]["name"] = name;
+        colordata["colors"][i - 1]["red"] = colortype[i][0];
+        colordata["colors"][i - 1]["green"] = colortype[i][1];
+        colordata["colors"][i - 1]["blue"] = colortype[i][2];
+      }
+      // store lights
+      colordata["lights"]["ambient"] = image->ambientColor[0];
+      colordata["lights"]["key"] = image->keyLightColor[0];
+      colordata["lights"]["fill"] = image->fillLightColor[0];
+      colordata["lights"]["back"] = image->backLightColor[0];
+      auto formatted = colordata.dump(4);
+      (void) fputs(formatted.c_str(), fp);
+    }
+    return 2;
+  }
+
+  if (strcmp(arg[0], "loadcolors") == 0) {
+    if (narg < 2) utils::missing_cmd_args(FLERR, "dump_modify loadcolors", error);
+
+    json colordata;
+    std::vector<uint8_t> jsondata;
+    int jsondata_size = 0;
+
+    if (comm->me == 0) {
+      SafeFilePtr fp = fopen(arg[1], "r");
+      if (fp == nullptr)
+        error->one(FLERR, argoff + 1, "Cannot open color JSON file {} for reading: {}", arg[1],
+                   utils::getsyserror());
+      try {
+        // try to parse as a JSON file. parser throws an exception on errors
+        // if successful, temporarily serialize to bytearray for communication
+        colordata = json::parse(fp);
+        jsondata = json::to_ubjson(colordata);
+        jsondata_size = jsondata.size();
+      } catch (std::exception &e) {
+        error->one(FLERR, argoff + 1, "Error parsing color JSON file {}: {}", arg[1], e.what());
+      }
+    }
+    MPI_Bcast(&jsondata_size, 1, MPI_INT, 0, world);
+
+    if (jsondata_size > 0) {
+
+      // broadcast binary JSON data to all processes and deserialize again
+
+      if (comm->me != 0) jsondata.resize(jsondata_size);
+      MPI_Bcast(jsondata.data(), jsondata_size, MPI_CHAR, 0, world);
+
+      // convert back to json class on all processors and free temporary storage
+      colordata.clear();
+      colordata = json::from_ubjson(jsondata);
+      jsondata.clear();    // free binary data
+
+      // process JSON data
+      if (colordata.contains("application")) {
+        if (colordata["application"] != "LAMMPS")
+          error->all(FLERR, argoff + 1, "JSON color file {} is for incompatible application: {}",
+                     arg[1], std::string(colordata["application"]));
+      } else {
+        error->all(FLERR, argoff + 1,
+                   "JSON color file {} does not contain required \"application\" field", arg[1]);
+      }
+      if (colordata.contains("format")) {
+        if (colordata["format"] != "colors")
+          error->all(FLERR, argoff + 1, "JSON file {} does not contain colors: {}", arg[1],
+                     std::string(colordata["format"]));
+      } else {
+        error->all(FLERR, argoff + 1,
+                   "JSON color file {} does not contain required \"format\" field", arg[1]);
+      }
+      if (colordata.contains("revision")) {
+        int rev = colordata["revision"];
+        if ((rev < 1) || (rev > 1))
+          error->all(FLERR, argoff + 1, "JSON color file {} with unsupported revision {}", arg[1],
+                     rev);
+      } else {
+        error->all(FLERR, argoff + 1,
+                   "JSON color file {} does not contain required \"revision\" field", arg[1]);
+      }
+
+      if (!colordata.contains("colors"))
+        error->all(FLERR, argoff + 1,
+                   "JSON color file {} does not contain required \"colors\" field", arg[1]);
+
+      int ncolors = colordata["colors"].size();
+      if (comm->me == 0) {
+        std::string title;
+        if (colordata.contains("title"))
+          title = std::string(": ") + std::string(colordata["title"]);
+        utils::logmesg(lmp, "Read JSON color file {} with {} colors{}\n", arg[1], ncolors, title);
+      }
+
+      // reset all named colors from JSON data
+      for (const auto &c : colordata["colors"]) {
+        if (!c.contains("name")) continue;
+        std::string name = c["name"];
+        double r = 0.0;
+        double g = 0.0;
+        double b = 0.0;
+        if (c.contains("red")) r = c["red"];
+        if (c.contains("green")) g = c["green"];
+        if (c.contains("blue")) b = c["blue"];
+        image->addcolor(name, r, g, b);
+      }
+      // store all per-type colors, create additional named colors, if needed
+      for (int itype = 1; itype <= atom->ntypes; ++itype) {
+        std::string name = fmt::format("type{}", itype);
+        double r = 0.0;
+        double g = 0.0;
+        double b = 0.0;
+        int i = (itype - 1) % ncolors;
+        const auto &c = colordata["colors"][i];
+        // if we have more types than colors, don't use the name but use type<itype>
+        if (itype <= ncolors)
+          if (c.contains("name")) name = c["name"];
+        if (c.contains("red")) r = c["red"];
+        if (c.contains("green")) g = c["green"];
+        if (c.contains("blue")) b = c["blue"];
+        image->addcolor(name, r, g, b);
+        colortype[itype] = image->color2rgb(name);
+      }
+      // apply lights, if present
+      if (colordata.contains("lights")) {
+        savedLights lights;
+        lights.ambient = image->ambientColor[0];
+        lights.key = image->keyLightColor[0];
+        lights.fill = image->fillLightColor[0];
+        lights.back = image->backLightColor[0];
+        if (colordata["lights"].contains("ambient"))
+          lights.ambient = colordata["lights"]["ambient"];
+        if (colordata["lights"].contains("key")) lights.key = colordata["lights"]["key"];
+        if (colordata["lights"].contains("fill")) lights.fill = colordata["lights"]["fill"];
+        if (colordata["lights"].contains("back")) lights.back = colordata["lights"]["back"];
+        restore_lighting(lights, image);
+      }
+    } else {
+      error->all(FLERR, argoff + 1, "Color file {} does not contain JSON data", arg[1]);
+    }
+    return 2;
   }
 
   return 0;
