b34e276d0c32a813c499dd39346a8483fb7123ea
Alexander Knoll <alexander.knoll@rub.de>
2026-05-21 15:02:45 +0200

    BUILD: update RuNNer tag.

--- cmake/Modules/Packages/ML-RUNNER.cmake
+++ cmake/Modules/Packages/ML-RUNNER.cmake
@@ -70,7 +70,7 @@ if(DOWNLOAD_RUNNER)
 
   ExternalProject_Add(runner_build
     GIT_REPOSITORY "https://gitlab.com/runner-suite/runner2.git"
-    GIT_TAG "2.0.1-20260519"
+    GIT_TAG "2.0.2_20260521"
     GIT_SHALLOW YES
     GIT_PROGRESS YES
 
