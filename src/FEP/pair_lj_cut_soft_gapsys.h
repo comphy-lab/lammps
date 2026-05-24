b2a4ac5c3bb2a19f453cdfe5b57d263eeda07d40
Evangelos Voyiatzis <evoyiatzis@gmail.com>
2026-05-24 19:38:51 +0200

    Remove init_style method from PairLJCutSoftGapsys

--- src/FEP/pair_lj_cut_soft_gapsys.h
+++ src/FEP/pair_lj_cut_soft_gapsys.h
@@ -31,7 +31,6 @@ class PairLJCutSoftGapsys : public Pair {
   void compute(int, int) override;
   void settings(int, char **) override;
   void coeff(int, char **) override;
-  void init_style() override;
   double init_one(int, int) override;
   void write_restart(FILE *) override;
   void read_restart(FILE *) override;
